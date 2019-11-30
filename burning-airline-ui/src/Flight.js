32b7efb93331c27163d765d0f860836482c8fb67
Pitt <pitt.rasananda@gmail.com>
2019-11-30 11:01:23 +1100

    display flights in UI

--- burning-airline-ui/src/Flight.js
+++ burning-airline-ui/src/Flight.js
@@ -1,17 +1,49 @@
 import React from 'react';
+import axios from 'axios';
+
+const SERVER_URL = 'http://localhost:3000/';
 
 class Flight extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            flights: []
+        };
+        const fetchFlights = () => {
+            axios.get(`${SERVER_URL}flights.json`)
+            .then(
+               results => this.setState(
+                   {
+                       flights: results.data
+                   }
+               ) 
+            )
+        }
+        fetchFlights();
+    }
     render() {
         return(
             <div>
                 <h2>Flights</h2>
-                <button>Create Flight</button>
-                
-                <label>Flight #</label>
-                <input />
+                <FlightsList flights={this.state.flights} />
             </div>
         )
     }
+}
+
+function FlightsList(props) {
+    return(
+        <div>
+            {
+                props.flights.map( f => ( <div key= {f.id}>
+                    <p>{f.date}</p>
+                    <p>{f.flight_number}</p>
+                    <p>{f.origin}</p>
+                    <p>{f.destination}</p>
+                </div>))
+            }
+        </div>
+    )
 }
 
 export default Flight;
