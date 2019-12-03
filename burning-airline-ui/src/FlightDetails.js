bbf5f38055c656f2389d816c20d9a5526a0b09a3
Se Oh <seoh@Ses-MacBook-Pro.local>
2019-12-03 20:56:39 +1100

    031219-1

--- burning-airline-ui/src/FlightDetails.js
+++ burning-airline-ui/src/FlightDetails.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import axios from 'axios';
+
+const SERVER_URL = 'http://localhost:3000/';
 
 class FlightDetails extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            flights: {},
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`${SERVER_URL}flights/${this.props.flightId}.json`)
+            .then(results => {
+                this.setState({flights: results.data});
+            });
+        }
 
     render() {
         return(
             <div>
-                <h3 class="text-left">Flight :{this.props.flightId} </h3>
-                
+                <h3 class="text-left">Flight :{this.props.flight} </h3>
+                <h3 class="text-left">Flight :{this.props.flight_Number} </h3>
             </div>
         )
     }
