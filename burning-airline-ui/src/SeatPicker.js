e06c2ec8d0c363a51695e19e24e0a8134f86ac4d
Se Oh <seoh@Ses-MacBook-Pro.local>
2019-12-07 09:14:28 +1100

    071219-2

--- burning-airline-ui/src/SeatPicker.js
+++ burning-airline-ui/src/SeatPicker.js
@@ -1,15 +1,28 @@
-import React from 'react';
+// import React from 'react';
 
-const seats = [
-    ["row 0 seat 0", "row 0 seat 1", "row 0 seat 2"],
-    ["row 1 seat 0", "row 1 seat 1", "row 1 seat 2"],
-    ["row 2 seat 0", "row 2 seat 1", "row 2 seat 2"],
-];
+// class SeatPicker extends React.Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             seats =[
+//                 ["row 0 seat 0", "row 0 seat 1", "row 0 seat 2"],
+//                 ["row 1 seat 0", "row 1 seat 1", "row 1 seat 2"],
+//                 ["row 2 seat 0", "row 2 seat 1", "row 2 seat 2"],
+//             ];
+//             seatsAvailable =[
+//                 ["row 0 seat 0", "row 0 seat 1", "row 0 seat 2"],
+//                 ["row 1 seat 0", "row 1 seat 1", "row 1 seat 2"],
+//                 ["row 2 seat 0", "row 2 seat 1", "row 2 seat 2"],
+//             ];
+//             seatReserved = []
+//         }
+//     }
+// }
 
-for (row of seats) {
-    for (column of row) {
-        console.log(column);
-    }
-}
+// for (row of seats) {
+//     for (column of row) {
+//         console.log(column);
+//     }
+// }
 
-export default 
+// export default 
